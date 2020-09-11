58c5b4e337c17c15ef417bc14c63398d09af1ff2
AtlanPrime <zilliondollarkuruvi@gmail.com>
2020-09-11 03:10:23 +0000

    bootleg_miatoll

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixys_miatoll.mk
+    $(LOCAL_DIR)/bootleg_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_miatoll-user \
-    pixys_miatoll-userdebug \
-    pixys_miatoll-eng
+    bootleg_miatoll-user \
+    bootleg_miatoll-userdebug \
+    bootleg_miatoll-eng
